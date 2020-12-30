349c3ae6e76c6bd5622e50d9f9cf6752dd0df23b
Rollen S. D'Souza <rollensdsouza@gmail.com>
2020-12-29 22:59:11 -0500

    Website Skeleton is Ready for Initial Publish.
    Additions:
     - Basic Webpages (Contact, Intro)
     - Posts from the past are functional...
     - Photo of me.

--- site.hs
+++ site.hs
@@ -2,12 +2,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
-
+import           Hakyll.Core.Compiler.Internal (compilerThrow)
+import           Data.Text (unpack, append, empty, singleton)
+import           Text.Pandoc
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
+    match "images/**" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "scripts/*" $ do
         route   idRoute
         compile copyFileCompiler
 
@@ -15,22 +21,23 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match "scripts/*" $ do
-        route   idRoute
-        compile copyFileCompiler
 
-    match (fromList ["about.tex", "contact.markdown"]) $ do
+    match (fromList ["about.tex", "contactme.md"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
-    match "posts/*" $ do
+    match "posts/*.markdown" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
+
+    match "posts/*.tex" $ do
+        route $ setExtension "html"
+        compile laTeXPostCompiler
 
     create ["archive.html"] $ do
         route idRoute
@@ -38,14 +45,13 @@ main = hakyll $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let archiveCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
+                    constField "title" "Archive"             `mappend`
                     defaultContext
 
             makeItem ""
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                 >>= relativizeUrls
-
 
     match "index.tex" $ do
       route   $ setExtension "html"
@@ -67,3 +73,48 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+--------------------------------------------------------------------------------
+laTeXPostCompiler :: Compiler (Item String)
+laTeXPostCompiler =
+  let
+    laTeXWriterOptions =
+      WriterOptions{
+        writerTemplate         = writerTemplate defaultHakyllWriterOptions
+      , writerVariables        = writerVariables defaultHakyllWriterOptions
+      , writerTabStop          = writerTabStop defaultHakyllWriterOptions
+      , writerTableOfContents  = writerTableOfContents defaultHakyllWriterOptions
+      , writerIncremental      = writerIncremental defaultHakyllWriterOptions
+      , writerHTMLMathMethod   = MathJax ""
+      , writerNumberSections   = writerNumberSections defaultHakyllWriterOptions
+      , writerNumberOffset     = writerNumberOffset defaultHakyllWriterOptions
+      , writerSectionDivs      = writerSectionDivs defaultHakyllWriterOptions
+      , writerExtensions       = writerExtensions defaultHakyllWriterOptions
+      , writerReferenceLinks   = writerReferenceLinks defaultHakyllWriterOptions
+      , writerDpi              = writerDpi defaultHakyllWriterOptions
+      , writerWrapText         = writerWrapText defaultHakyllWriterOptions
+      , writerColumns          = writerColumns defaultHakyllWriterOptions
+      , writerEmailObfuscation = writerEmailObfuscation defaultHakyllWriterOptions
+      , writerIdentifierPrefix = writerIdentifierPrefix defaultHakyllWriterOptions
+      , writerCiteMethod       = writerCiteMethod defaultHakyllWriterOptions
+      , writerHtmlQTags        = writerHtmlQTags defaultHakyllWriterOptions
+      , writerSlideLevel       = writerSlideLevel defaultHakyllWriterOptions
+      , writerTopLevelDivision = writerTopLevelDivision defaultHakyllWriterOptions
+      , writerListings         = writerListings defaultHakyllWriterOptions
+      , writerHighlightStyle   = writerHighlightStyle defaultHakyllWriterOptions
+      , writerSetextHeaders    = writerSetextHeaders defaultHakyllWriterOptions
+      , writerEpubSubdirectory = writerEpubSubdirectory defaultHakyllWriterOptions
+      , writerEpubMetadata     = writerEpubMetadata defaultHakyllWriterOptions
+      , writerEpubFonts        = writerEpubFonts defaultHakyllWriterOptions
+      , writerEpubChapterLevel = writerEpubChapterLevel defaultHakyllWriterOptions
+      , writerTOCDepth         = writerTOCDepth defaultHakyllWriterOptions
+      , writerReferenceDoc     = writerReferenceDoc defaultHakyllWriterOptions
+      , writerReferenceLocation= writerReferenceLocation defaultHakyllWriterOptions
+      , writerSyntaxMap        = writerSyntaxMap defaultHakyllWriterOptions
+      , writerPreferAscii      = writerPreferAscii defaultHakyllWriterOptions
+      }
+  in do
+    pandocCompilerWith defaultHakyllReaderOptions laTeXWriterOptions
+      >>= loadAndApplyTemplate "templates/post.html" postCtx
+      >>= loadAndApplyTemplate "templates/default.html" postCtx
+      >>= relativizeUrls
